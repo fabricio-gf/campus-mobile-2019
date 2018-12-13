ad4f94942e57b018ad045b6a396984011d94ca59
Fabricio <fabricio-gf@hotmail.com>
2018-12-13 19:23:24 -0200

    Added menu button animation system

---
+++ Assets/Scripts/Menu/MenuAnimations.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class MenuAnimations : MonoBehaviour {
+
+	private Animator animator;
+
+	void Awake () {
+		animator = GetComponent<Animator>();		
+	}
+
+	public void TriggerAnimation(string trigger){
+		animator.SetTrigger(trigger);
+	}
+}
